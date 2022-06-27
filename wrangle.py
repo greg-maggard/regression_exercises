41d31087c91688d0487ac293609bf26f831858b4
Greg Maggard <gregamaggard@gmail.com>
2022-06-26 19:07:30 -0500

    Completed some more of the regression exercises.

--- wrangle.py
+++ wrangle.py
@@ -1,6 +1,24 @@
-from acquire import get_zillow_data
 import numpy as np
 import pandas as pd
+import os
+
+def get_zillow_data():
+    filename = "zillow.csv"
+
+    if os.path.isfile(filename):
+        return pd.read_csv(filename, index_col = 0)
+    else:
+        # read the SQL query into a dataframe
+        df = pd.read_sql('''SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips, propertylandusedesc 
+        FROM properties_2017 
+        JOIN propertylandusetype on properties_2017.propertylandusetypeid = propertylandusetype.propertylandusetypeid 
+        WHERE propertylandusedesc = "Single Family Residential";''', get_db_url('zillow'))
+
+        # Write that dataframe to disk for later. Called "caching" the data for later.
+        df.to_csv(filename)
+
+        # Return the dataframe to the calling code
+        return df
 
 def clearing_fips(df):
     '''This function takes in a DataFrame of unprepared Zillow information and generates a new
